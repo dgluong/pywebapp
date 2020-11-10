fb7ddafdce0b7acefa963d55c0773fcd09665a30
Daniel Luong <43552873+dgluong@users.noreply.github.com>
2020-11-10 10:04:48 -0800

    Update startcontainer.sh
    updated

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 170015466808.dkr.ecr.us-west-2.amazonaws.com
-docker pull 170015466808.dkr.ecr.us-west-2.amazonaws.com/farnazdargahi/pywebapp:latest || {
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 131301854901.dkr.ecr.us-west-2.amazonaws.com
+docker pull 131301854901.dkr.ecr.us-west-2.amazonaws.com/dgluong/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
